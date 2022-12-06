4a174d93ed472ececaf482696f5518042cc0d08f
Federico Zuccollo <federico.zuccollo@gmail.com>
2022-12-06 13:58:53 +0100

    Repeated characters
    Finding repetitions in a sequence.

--- 2022/06/Program.cs
+++ 2022/06/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var line = File.ReadAllText(@"../../../input.txt");
+
+int length = 14; // part 1: 4; part 2: 14
+var marker = new char[length];
+int at = 0, ix = 0;
+
+for(; ix<line.Length && at<length; ) {
+	var ch = line[ix++];
+	var pos = Array.IndexOf(marker, ch, 0, at);
+
+	if (pos >= 0) {
+		at -= pos + 1;
+		Array.Copy(marker, pos+1, marker, 0, at);
+	}
+
+	marker[at++] = ch;
+}
+
+Console.WriteLine(ix);
