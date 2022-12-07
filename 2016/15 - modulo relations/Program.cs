66e3576bf14f73ad2fefc1eb27df8aaf844e216d
Federico Zuccollo <federico.zuccollo@gmail.com>
2022-12-07 10:55:51 +0100

    Zero modulo sum of dependent variables.

---
+++ 2016/15 - modulo relations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines(@"../../../input.txt");
+Regex lexer = new(@"Disc #(\d+) has (\d+) positions; at time=(\d+), it is at position (\d+).");
+
+var positions = lines.Select(l => {
+	var m = lexer.Match(l);
+	return new Disk(parse(1), parse(2), parse(4));
+
+	int parse(int gn) => int.Parse(m.Groups[gn].ValueSpan);
+}).ToArray();
+
+int time, max_time = 10000000;
+
+for(time=0; time < max_time; time++) {
+	var sum = positions.Sum(d => d.new_pos(time));
+
+	if (sum == 0) break;
+}
+
+Console.WriteLine(time);
+
+record Disk(int id, int maxPosition, int position) {
+	public int new_pos(int time) => (time + id + position) % maxPosition;
+}
