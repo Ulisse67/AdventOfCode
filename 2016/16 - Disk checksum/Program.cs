98c6df7f94e7b258da5a306a9f9663028fc88f86
Federico Zuccollo <federico.zuccollo@gmail.com>
2022-12-09 00:09:00 +0100

    Disk checksum with large strings.

---
+++ 2016/16 - Disk checksum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+var input = "10011111011011001";
+var length = 35651584;// 272;
+var buffer = new StringBuilder(input, length);
+
+while(buffer.Length < length) {
+	Span<char> dup = new(buffer.ToString().ToCharArray());
+	for (int i = 0; i <= dup.Length / 2; i++) {
+		var ch = xor(dup[i]);
+		dup[i] = xor(dup[^(1+i)]);
+		dup[^(1+i)] = ch;
+
+		static char xor(char ch) => (char)('0'+(1 - (ch - '0')));
+	}
+
+	buffer.Append('0').Append(dup);
+}
+
+var checksum = new char[buffer.Length / 2];
+var digits = buffer.ToString(0, length);
+
+for (; ; ) {
+	int dst = 0;
+	for (int at = 0; at < length; at += 2) {
+		var ch = digits[at..(at + 2)] switch {
+			"00" => '1',
+			"01" => '0',
+			"10" => '0',
+			"11" => '1',
+		};
+
+		checksum[dst++] = ch;
+	}
+
+	if ((dst & 1) == 1) {
+		ReadOnlySpan<char> res = new(checksum, 0, dst);
+		Console.WriteLine(res.ToString());
+
+		break;
+	}
+
+	length = dst;
+	digits = new string(checksum, 0, length);
+}
