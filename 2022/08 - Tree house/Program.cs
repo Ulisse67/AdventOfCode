8ac7ea6e15262c095815e7f569b7208c800bedf1
Federico Zuccollo <federico.zuccollo@gmail.com>
2022-12-08 08:57:54 +0100

    Tree house
    Looking in the 4 directions for scores.

---
+++ 2022/08 - Tree house/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Xml.Schema;
+
+using Extensions;
+
+var lines = File.ReadAllLines(@"../../../input.txt");
+var grid = lines.Make<char,string>();
+var (maxrows,maxcols) = (grid.GetLength(0), grid.GetLength(1));
+
+int visible_trees = 2 * (maxrows + maxcols) -4; // don't sum corners twice
+
+for(int row=1; row<maxrows-1; row++) {
+	for(int col=1; col<maxcols-1; col++) {
+		var ch = grid[row, col];
+
+		if(left() || right() || up() || down())
+			visible_trees++;
+
+		bool left()
+		{
+			for (int x = col - 1; x >= 0; x--) if (grid[row, x] >= ch) return false;
+			return true;
+		}
+
+		bool right()
+		{
+			for (int x = col + 1; x < maxcols; x++) if (grid[row, x] >= ch) return false;
+			return true;
+		}
+
+		bool up()
+		{
+			for (int y = row -1; y >=0; y--) if (grid[y, col] >= ch) return false;
+			return true;
+		}
+
+		bool down()
+		{
+			for (int y = row + 1; y < maxrows; y++) if (grid[y, col] >= ch) return false;
+			return true;
+		}
+	}
+}
+
+Console.WriteLine("Visible trees: {0}", visible_trees);
+
+// Part 2
+//
+int max_score = 0;
+
+for (int row = 1; row < maxrows-1; row++) {
+	for (int col = 1; col < maxcols-1; col++) {
+		var ch = grid[row, col];
+
+		var score = left() * right() * up() * down();
+		if(score>max_score) max_score = score;
+
+		int left()
+		{
+			int x;
+			for (x = col - 1; x >= 0; x--) if (grid[row, x] >= ch) return col-x;
+			return col;
+		}
+
+		int right()
+		{
+			int x;
+			for (x = col + 1; x < maxcols; x++) if (grid[row, x] >= ch) return x-col;
+			return maxcols - (col+1);
+		}
+
+		int up()
+		{
+			int y;
+			for (y = row - 1; y >= 0; y--) if (grid[y, col] >= ch) return row-y;
+			return row;
+		}
+
+		int down()
+		{
+			int y;
+			for (y = row + 1; y < maxrows; y++) if (grid[y, col] >= ch) return y-row;
+			return maxrows - (row+1);
+		}
+	}
+}
+
+Console.WriteLine("Max score: {0}", max_score);
---
+++ 2022/Extensions/Matrix.cs
@@ -0,0 +1,48 @@
+﻿namespace Extensions;
+
+public static class MatrixExtensions {
+	public static T[,] Make<T, S>(this IEnumerable<S> lines) where S : IEnumerable<T>
+	{
+		var (rows, cols) = (lines.Count(), lines.First().Count());
+
+		T[,] matrix = new T[rows, cols];
+		int r = 0;
+		foreach (var l in lines) {
+			int c = 0;
+			foreach (var ch in l) matrix[r, c++] = ch;
+			r++;
+		}
+
+		return matrix;
+	}
+
+	public static T[,] Fill<T>(this T[,] ar, T val)
+	{
+		int d0 = ar.GetLength(0), d1 = ar.GetLength(1);
+		for (int r = 0; r < d0; r++) for (int c = 0; c < d1; c++) ar[r, c] = val;
+
+		return ar;
+	}
+
+	static T[,] Clone<T>(this T[,] lhs)
+	{
+		T[,] res = new T[lhs.GetLength(0), lhs.GetLength(1)];
+		for (int r = 0; r < lhs.GetLength(0); r++)
+			for (int c = 0; c < lhs.GetLength(1); c++) res[r, c] = lhs[r, c];
+
+		return res;
+	}
+
+	public static void Print<T>(this T[,] mx)
+	{
+		int d0 = mx.GetLength(0), d1 = mx.GetLength(1);
+		for (int r = 0; r < d0; r++) {
+			Console.Write($"{r,5}: ");
+			for (int c = 0; c < d1; c++) {
+				Console.Write(mx[r, c]);
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
