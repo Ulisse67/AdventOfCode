cb57a626f2d90100374570b5e34679d9a36998b9
Federico Zuccollo <federico.zuccollo@gmail.com>
2022-12-22 19:32:53 +0100

    Update Program.cs
    Seen a different solution (in Python) and used it to speed up my code.
    AoC is a very good school.

--- 2022/16 - Valve pressure release/Program.cs
+++ 2022/16 - Valve pressure release/Program.cs
@@ -1,29 +1,29 @@
-﻿using System.ComponentModel;
+﻿#define FAST
+
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
-using System.IO.Compression;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 using Extensions;
 
 
-var lines = File.ReadAllLines(@"../../../input.txt");
+var lines = File.ReadAllLines(@"../../../input2.txt");
 
 var valves = lines.Select(l => Valve.Parse(l)).ToDictionary(v => v.key);
 var paths = compute_shortest_paths(valves);
 var flows = valves.Values.Where(v => v.pressure > 0).ToArray();
 
+const int MAXTIME = 30;
+
+#if SLOW
 int bitlen = 1 << flows.Length;
 uint allbits = (uint)(bitlen - 1);
 
 // Part1
 //
-const int MAXTIME = 30;
-
 var start = new ValveState(valves["AA"],visitTime: -1,openTime: 0,0,null);
-int score = compute_pressure(start,MAXTIME,flows,allbits);
+int score = compute_pressure_slow(start,MAXTIME,flows,allbits);
 
 Console.WriteLine("Max pressure from {0} valves: {1}", flows.Length, score);
 
@@ -38,7 +38,7 @@ Dictionary<uint,int> scores_from_masks = new(bitlen);
 // Try all bit combinations (
 //
 for(uint m=0; m<bitlen; m++) {
-	var sc = compute_pressure(start,MAXTIME-4,flows,m);
+	var sc = compute_pressure_slow(start,MAXTIME-4,flows,m);
 	scores_from_masks[m] = sc;
 }
 
@@ -55,9 +55,52 @@ for(uint m = 0; m < bitlen - 1; m++) {
 }
 
 Console.WriteLine("Max pressure from {0} valves operated in 2 groups: {1}",flows.Length,score);
+#endif
 
 
-int compute_pressure(ValveState start, int maxtime, IEnumerable<Valve> valveset, uint mask)
+// Learned a faster way to keep information using a mask
+// and avoiding computing it again for different sets of valves
+
+#if FAST
+Dictionary<uint,int> answers = new();
+
+compute_pressure(valves["AA"],MAXTIME,0,0);
+
+var maxscore = answers.Values.Max();
+
+Console.WriteLine(maxscore);
+
+answers.Clear();
+compute_pressure(valves["AA"],MAXTIME - 4,0,0);
+
+var fn = from s0 in answers
+				 from s1 in answers
+				 where (s0.Key & s1.Key) == 0 // only sums of different valves
+				 select s0.Value + s1.Value;
+
+maxscore = fn.Max();
+
+Console.WriteLine(maxscore);
+
+
+void compute_pressure(Valve v, int maxtime, uint state, int totalOutput)
+{
+	answers[state] = Math.Max(answers.GetValueOrDefault(state,0), totalOutput);
+
+	foreach(var vv in flows) {
+		if((vv.BitMask & state) != 0) continue; // already visited
+
+		var newbudget = maxtime - paths![v.Index, vv.Index] - 1;
+
+		if(newbudget <= 0) continue;
+
+		compute_pressure(vv, newbudget, state | vv.BitMask, totalOutput + newbudget * vv.pressure);
+	}
+}
+#endif
+
+#if SLOW
+int compute_pressure_slow(ValveState start, int maxtime, IEnumerable<Valve> valveset, uint mask)
 {
 	Queue<ValveState> processing = new();
 	int maxscore = 0;
@@ -69,7 +112,7 @@ int compute_pressure(ValveState start, int maxtime, IEnumerable<Valve> valveset,
 		maxscore = Math.Max(maxscore,output);
 
 		foreach(var v in valveset) {
-			if((mask & 1 << v.BitIndex) == 0) continue;
+			if((mask & v.BitMask) == 0) continue;
 
 			if(st.FindFor(v) != null) continue;
 
@@ -85,6 +128,8 @@ int compute_pressure(ValveState start, int maxtime, IEnumerable<Valve> valveset,
 
 	return maxscore;
 }
+#endif
+
 
 static int[,] compute_shortest_paths(IReadOnlyDictionary<string,Valve> dict)
 {
@@ -129,18 +174,19 @@ record class Valve(string key, int pressure, string[] successors) {
 		var vlist = m.Groups[4].Captures.Select(c=>c.Value).ToArray();
 
 		var v = new Valve(m.Groups[1].Value, int.Parse(m.Groups[2].ValueSpan), vlist);
-		if(v.pressure > 0) v.BitIndex = maskIndex++;
+		if(v.pressure > 0) v.BitMask = 1U << maskIndex++;
 		return v;
 	}
 
 	public readonly int Index = creationIndex++;
-	public int BitIndex;
+	public uint BitMask;
 
 	static int creationIndex, maskIndex;
 
 	static readonly Regex matcher = new(@"Valve (\w\w) has flow rate=(\d+);.+valve[s]? ((\w\w)(, )?)+");
 }
 
+#if SLOW
 record class ValveState(Valve valve, int visitTime, int openTime, int totalOutput, ValveState? parent) {
 	public int Output => valve.pressure * openTime;
 
@@ -150,3 +196,4 @@ record class ValveState(Valve valve, int visitTime, int openTime, int totalOutpu
 			return null;
 	}
 }
+#endif
